87401c85c7f886308973a5728b13d89637917953
postmodern <postmodern.mod3@gmail.com>
2010-04-30 21:15:35 -0700

    Use the new 'dm-core/spec/setup' file.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -12,3 +12,13 @@ end
 require 'spec'
 require 'dm-core/spec/setup'
 require 'dm-is-read_only'
+
+DataMapper::Spec.setup
+
+Spec::Runner.configure do |config|
+  config.extend(DataMapper::Spec::Adapters::Helpers)
+
+  config.after :all do
+    DataMapper::Spec.cleanup_models
+  end
+end
