79f434eb259b101c8efd304c65ca6f3637683b44
postmodern <postmodern.mod3@gmail.com>
2010-05-20 18:35:57 -0700

    Updated the spec_helper.rb for DataMapper 1.0.0.rc2.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -11,14 +11,12 @@ end
 
 require 'spec'
 require 'dm-core/spec/setup'
+require 'dm-core/spec/lib/adapter_helpers'
+
 require 'dm-is-read_only'
 
 DataMapper::Spec.setup
 
 Spec::Runner.configure do |config|
   config.extend(DataMapper::Spec::Adapters::Helpers)
-
-  config.after :all do
-    DataMapper::Spec.cleanup_models
-  end
 end
